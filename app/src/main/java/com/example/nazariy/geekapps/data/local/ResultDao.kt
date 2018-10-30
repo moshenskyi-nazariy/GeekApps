0136f31941039a8ed399fdd7ecc4e3c4dd4037df
Nazarii Moshenskyi <moshenskyi.nazariy@gmail.com>
2018-10-30 21:49:38 +0200

    Shortened with apply function.

--- app/src/main/java/com/example/nazariy/geekapps/data/local/ResultDao.kt
+++ app/src/main/java/com/example/nazariy/geekapps/data/local/ResultDao.kt
@@ -14,68 +14,88 @@ private const val FIELD_KIND_AUDIOBOOK = "book"
 
 class ResultDao(private val realmDb: Realm) {
     fun saveFavourites(favourite: Result) {
-        realmDb.beginTransaction()
-        val realmResult = ResultRealm(favourite)
-        realmDb.copyToRealmOrUpdate(realmResult)
-        realmDb.commitTransaction()
+        realmDb.apply {
+            beginTransaction()
+            val realmResult = ResultRealm(favourite)
+            copyToRealmOrUpdate(realmResult)
+            commitTransaction()
+        }
     }
 
     fun getFavourites(): List<Result> {
-        realmDb.beginTransaction()
-        val results = realmDb.where(ResultRealm::class.java)
-                .equalTo(FIELD_IS_CHECKED, true)
-                .sort(FIELD_KIND)
-                .findAll()
-        realmDb.commitTransaction()
+        var results: RealmResults<ResultRealm>? = null
+
+        realmDb.apply {
+            beginTransaction()
+            results = where(ResultRealm::class.java)
+                        .equalTo(FIELD_IS_CHECKED, true)
+                        .sort(FIELD_KIND)
+                        .findAll()
+            commitTransaction()
+        }
         return getList(results)
     }
 
-    private fun getList(realmList: RealmResults<ResultRealm>): List<Result> {
-        val realmResultList = realmList.toList()
+    private fun getList(realmList: RealmResults<ResultRealm>?): List<Result> {
+        val realmResultList = realmList?.toList()
         val resultList = ArrayList<Result>()
-        for (result in realmResultList) {
-            val domainResultEntity = Result(result)
-            resultList.add(domainResultEntity)
+        realmResultList?.let {
+            for (result in realmResultList) {
+                val domainResultEntity = Result(result)
+                resultList.add(domainResultEntity)
+            }
         }
 
         return resultList
     }
 
     fun getAudioBooks(): List<Result> {
-        realmDb.beginTransaction()
-        val results = realmDb
-                .where(ResultRealm::class.java)
-                .equalTo(FIELD_KIND, FIELD_KIND_AUDIOBOOK)
-                .findAll()
-        realmDb.commitTransaction()
+        var results: RealmResults<ResultRealm>? = null
+
+        realmDb.apply {
+            beginTransaction()
+            results = where(ResultRealm::class.java)
+                    .equalTo(FIELD_KIND, FIELD_KIND_AUDIOBOOK)
+                    .findAll()
+            commitTransaction()
+        }
+
         return getList(results)
     }
 
     fun getMovies(): List<Result> {
-        realmDb.beginTransaction()
-        val results = realmDb
-                .where(ResultRealm::class.java)
-                .equalTo(FIELD_KIND, FIELD_KIND_MOVIE)
-                .findAll()
-        realmDb.commitTransaction()
+        var results: RealmResults<ResultRealm>? = null
+
+        realmDb.apply {
+            beginTransaction()
+            results = where(ResultRealm::class.java)
+                        .equalTo(FIELD_KIND, FIELD_KIND_MOVIE)
+                        .findAll()
+            commitTransaction()
+        }
         return getList(results)
     }
 
     fun getPodcasts(): List<Result> {
-        realmDb.beginTransaction()
-        val results = realmDb
-                .where(ResultRealm::class.java)
-                .equalTo(FIELD_KIND, FIELD_KIND_PODCAST)
-                .findAll()
-        realmDb.commitTransaction()
+        var results: RealmResults<ResultRealm>? = null
+
+        realmDb.apply {
+            beginTransaction()
+            results = where(ResultRealm::class.java)
+                        .equalTo(FIELD_KIND, FIELD_KIND_PODCAST)
+                        .findAll()
+            commitTransaction()
+        }
         return getList(results)
     }
 
     fun saveResults(results: List<Result>) {
-        realmDb.beginTransaction()
-        for (result in results) {
-            realmDb.copyToRealmOrUpdate(ResultRealm(result))
+        realmDb.apply {
+            beginTransaction()
+            for (result in results) {
+                copyToRealmOrUpdate(ResultRealm(result))
+            }
+            commitTransaction()
         }
-        realmDb.commitTransaction()
     }
 }
